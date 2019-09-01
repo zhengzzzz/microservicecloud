2f072093441cbfd6155779b0cf72bb4927be4d7b
zhangzheng <934363825@qq.com>
2019-09-01 13:38:33 +0800

    123

--- microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -25,6 +25,52 @@ public class Dept implements Serializable// entity --orm--- db_table
 		super();
 		this.dname = dname;
 	}
+
+	public Long getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(Long deptno) {
+		this.deptno = deptno;
+	}
+
+	public String getDname() {
+		return dname;
+	}
+
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+
+	public String getDb_source() {
+		return db_source;
+	}
+
+	public void setDb_source(String db_source) {
+		this.db_source = db_source;
+	}
+
+	public Dept(Long deptno, String dname, String db_source) {
+		super();
+		this.deptno = deptno;
+		this.dname = dname;
+		this.db_source = db_source;
+	}
+	
+	
+
+
+	public Dept() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Dept [deptno=" + deptno + ", dname=" + dname + ", db_source=" + db_source + "]";
+	}
+	
+	
+	
 	
 	
 }
